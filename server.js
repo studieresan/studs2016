c13665e025c2cfaffbb51856a1320e77a077f7da
William Perkola <perkola.william@gmail.com>
2015-11-29 14:38:51 +0100

    Update server.js docs

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-// server.js
+/*
+|------------------------------------------------------------------------------
+| Server modules
+|------------------------------------------------------------------------------
+*/
 
-// modules
 var bodyParser  = require('body-parser');
 var dotenv      = require('dotenv');
 var mongoose    = require('mongoose');
@@ -8,22 +11,37 @@ var express     = require('express');
 var flash       = require('connect-flash');
 var passport    = require('passport');
 var session     = require('express-session');
-var MongoStore 	= require('connect-mongo') (session);
-var app         = express();
+var MongoStore  = require('connect-mongo') (session);
 
-require('./server/config/passport') (passport);
+/*
+|------------------------------------------------------------------------------
+| Server configuration
+|------------------------------------------------------------------------------
+*/
 
-// confiuration
+// dotenv
 dotenv.load();
 
-// database configuration file
+// passport
+require('./server/config/passport') (passport);
+
+// databse
 var db = require('./server/config/db');
+
+// server port number
+var port = process.env.PORT || 3000;
 
 // connect to mongoDB
 mongoose.connect(db.url);
 
-// port number
-var port = process.env.PORT || 3000;
+/*
+|------------------------------------------------------------------------------
+| Application setup
+|------------------------------------------------------------------------------
+*/
+
+// create application
+var app = express();
 
 // parse application/json
 app.use(bodyParser.json());
@@ -34,27 +52,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // set location of static files
 app.use(express.static(__dirname + '/client/public'));
 
+// session setup
 app.use(session({
-   collection: 'sessions',
-   name: 'session',
-   resave: false,
-   saveUninitialized: true,
-   secret: process.env.SESSION_SECRET,
-   store: new MongoStore({ mongooseConnection: mongoose.connection }),
-   ttl: 2*24*60*60, //2 day before expiration
+	collection: 'sessions',
+	name: 'session',
+	resave: false,
+	saveUninitialized: true,
+	secret: process.env.SESSION_SECRET,
+	store: new MongoStore({ mongooseConnection: mongoose.connection }),
+	ttl: 2*24*60*60, // 2 day before expiration
 }));
+
+// passport setup
 app.use(passport.initialize());
+
+// passport session
 app.use(passport.session());
+
+// flash messaging
 app.use(flash());
 
-// Configure view templating
+// view template setup
 app.set('views', __dirname + '/server/views/');
-// Add jade as view engine
+
+// view engine setup
 app.set('view engine', 'jade');
 
-// routes
+// routing
 require('./server/routes') (app, express);
 
+// start server
 app.listen(port, function() {
-    console.log('Listening on port ' + port);
+	console.log('Listening on port ' + port);
 });
