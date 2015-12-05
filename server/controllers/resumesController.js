bc7f488fcc74628b7dad2041443311854b78ddfc
William Perkola <perkola.william@gmail.com>
2015-12-05 10:47:55 +0100

    Fix typo

--- server/controllers/resumesController.js
+++ server/controllers/resumesController.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var Resume = require('../models/Resume');
 
 exports.findAll = function(req, res) {
-	Resume.find({}).populate('user').exec(function (err, resumes) {
+	Resume.find({}).populate('student').exec(function (err, resumes) {
 		return res.send(resumes);
 	});
 };
