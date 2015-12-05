fd86230d7cc6ab8a20c40b27fa55aa693135ca3a
Victor Larsson <victor@dictodesigns.com>
2015-12-05 11:44:56 +0100

    Login now works great when successful

--- client/js/controllers/auth-controllers.js
+++ client/js/controllers/auth-controllers.js
@@ -2,13 +2,13 @@
 
 	var authControllers = angular.module('authControllers', []);
 
-	authControllers.controller("loginCtrl", ['$scope', '$http', '$location', function($scope, $http, $location) {
+	authControllers.controller("loginCtrl", ['$scope', '$http', function($scope, $http) {
 		// Do stuff like login user.
 		$scope.login = function(user) {
 			var email = user.email;
 			var password = user.password;
 			$http.post('/login', {'email':email, 'password': password}).then(function successCallback(response) {
-				alert("Logged in!");
+				window.location.replace("/");
 			}, function errorCallback(response) {
 				alert("Invalid credentials!");
 			});
