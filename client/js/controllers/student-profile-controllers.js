1a5fb9c437f3c9e79dfc5d42382a212db0595266
Victor Larsson <victor@dictodesigns.com>
2015-12-11 17:47:37 +0100

    User data editable!

--- client/js/controllers/student-profile-controllers.js
+++ client/js/controllers/student-profile-controllers.js
@@ -2,8 +2,15 @@
 
 	var studentProfileControllers = angular.module('studentProfileControllers', []);
 
-	studentProfileControllers.controller("userEditCtrl", ['$scope', function($scope) {
-		$scope.user = {};
+	studentProfileControllers.controller("userEditCtrl", ['$scope', '$http', 'Flash', function($scope, $http, Flash) {
+		$scope.edit = function(user) {
+			$http.put('/api/students', user).then(function successCallback(response) {
+				Flash.create('info', "Saved!");
+			}, function errorCallback(response) {
+				Flash.create('danger', "Something went bad. Try again!");
+				user.password = "";
+			});
+		};
 	}]);
 	
 })();
