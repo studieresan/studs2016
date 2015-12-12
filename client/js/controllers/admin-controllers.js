77018246be9baca8b9bce13241e039016ff7dc5d
Joakim Edlund <joakim.en.edlund@gmail.com>
2015-12-12 18:17:07 +0100

    Fixed so admins get a suggested password when registring a user.

--- client/js/controllers/admin-controllers.js
+++ client/js/controllers/admin-controllers.js
@@ -3,6 +3,7 @@
 	var adminControllers = angular.module('adminControllers', []);
 
 	adminControllers.controller("addStudentCtrl", ['$scope', '$http', 'Flash', function($scope, $http, Flash) {
+
 		$scope.add = function(user) {
 			$http.post('/api/students', user).then(function successCallback(response) {
 				Flash.create('info', "Registered!");
@@ -10,6 +11,15 @@
 				Flash.create('danger', "Something went bad. Try again!");
 			});
 		};
+
+		$scope.suggestPassword = function() {
+			var length = 10, charset = "abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", retVal = "";
+		    for (var i = 0, n = charset.length; i < length; ++i) {
+		        retVal += charset.charAt(Math.floor(Math.random() * n));
+		    }
+		    return retVal;
+		};
+		$scope.suggestedPassword = $scope.suggestPassword();
 	}]);
 	
 })();
