1fead24003d3e21d04d9c55e5ad2b91f4190059a
Victor Larsson <victor@dictodesigns.com>
2015-12-21 23:33:32 +0100

    Resumes now auto-saves every 30 seconds when editing + ux fix when empty

--- client/js/controllers/resume-controllers.js
+++ client/js/controllers/resume-controllers.js
@@ -2,40 +2,86 @@
 
 	var resumeControllers = angular.module('resumeControllers', []);
 
-	resumeControllers.controller("ResumeCtrl", ['$scope', 'Resume', '$routeParams', 'Flash', function($scope, Resume, $routeParams, Flash) {
+	resumeControllers.controller("ResumeCtrl", ['$scope', '$interval', 'Resume', '$routeParams', 'Flash', function($scope, $interval, Resume, $routeParams, Flash) {
 
 		$scope.data = {
 			editDescription: false,
-			activePost: null
+			activePost: null,
+			loading: true,
+			empty: true
 		};
 
-		console.log($scope.data.activePost);
-
 		Resume.get({ student: 'mine' }, function(data) {
+			// This will run only when data is loaded.
+			window.onbeforeunload = function (event) {
+				var message = 'Sure you want to leave?';
+				if (typeof event == 'undefined') {
+					event = window.event;
+				}
+				if (event) {
+					event.returnValue = message;
+				}
+				return message;
+			};
+
 			console.log(data);
 			$scope.Resume = data;
+			if($scope.Resume.posts.length < 1) {
+				// Push Work experience and education
+				$scope.Resume.posts.push({
+					type: "Work experience",
+					title: "Edit this!",
+					description: "Add description here.",
+					startdate: "2015-12-01",
+					enddate: "2016-12-01",
+					newItem: 'new'
+				});
+				$scope.Resume.posts.push({
+					type: "Education",
+					title: "Edit this!",
+					description: "Add description here.",
+					startdate: "2015-12-01",
+					enddate: "2016-12-01",
+					newItem: 'new'
+				});
+			}
+			$scope.data.loading = false;
 		});
 
 		$scope.setEditDescription = function (bool) {
 			$scope.data.editDescription = bool;
 		};
 
-		$scope.update = function (data) {
+		$scope.update = function (data, isAutoSave) {
 			console.log(data);
 			Resume.update({ student: 'mine' }, data, function successCallback(response) {
-				Flash.create('info', "Resumé saved!");
+				if(isAutoSave) {
+					Flash.create('info', "Auto-saving resume...");
+				} else {
+					Flash.create('info', "Resumé saved!");
+				}
 			}, function errorCallback(response) {
-				Flash.create('danger', "Something went bad. Try again!");
+				if(isAutoSave) {
+					Flash.create('danger', "Auto-save failed...");
+				} else {
+					Flash.create('danger', "Something went bad. Try again!");
+				}
 			});
 		};
+		// Auto-save every 30 second
+		$interval(function() {
+			$scope.update($scope.Resume, true);
+		}, 30000);
 
 		$scope.push = function (type) {
-			$scope.Resume.posts.push({
-				type: type,
-				title: "Edit this!",
-				description: "Add description here.",
-				newItem: 'new'
-			});
+			if(type !== undefined) {
+				$scope.Resume.posts.push({
+					type: type,
+					title: "Edit this!",
+					description: "Add description here.",
+					newItem: 'new'
+				});
+			}
 		};
 
 		$scope.removePost = function (post) {
@@ -57,4 +103,11 @@
 			$scope.newTypeValue = "";
 		};
 	}]);
+
+resumeControllers.controller("PublicResumeCtrl", ['$scope','Resume', '$routeParams', function($scope, Resume, $routeParams) {
+	Resume.get({ student: $routeParams.id }, function(data) {
+		console.log(data);
+		$scope.Resume = data;
+	});
+}]);
 })();
