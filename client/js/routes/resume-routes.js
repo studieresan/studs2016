aefc9a5318ee01d935dfbb92b2b4c2d63386c80d
Victor Larsson <victor@dictodesigns.com>
2015-12-21 23:36:25 +0100

    updated to new routes for resume

--- client/js/routes/resume-routes.js
+++ client/js/routes/resume-routes.js
@@ -1,8 +1,12 @@
 (function() {
   resumes.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.when('/resumes', {
+    $routeProvider.when('/profile/resume', {
       templateUrl: 'views/resumes/add-edit.html',
       controller: 'ResumeCtrl',
+    })
+    .when('/resume/:id', {
+      templateUrl: 'views/resumes/public.html',
+      controller: 'PublicResumeCtrl',
     })
     .otherwise({
       redirectTo: '/'
