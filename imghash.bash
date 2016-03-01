9fa1588bfefdd4c2dd38e90296ea8f98daba6661
William Perkola <perkola.william@gmail.com>
2016-03-01 13:26:05 +0100

    Move images

--- imghash.bash
+++ imghash.bash
@@ -6,6 +6,6 @@ do
         base=${file##*/}
         noext=${base%.*}
         newfile=$(printf '%s' "$noext" | openssl sha1)
-        cp "$file" "client/public/img/profile-pictures/hashed/$newfile.jpg"
+        cp "$file" "client/img/profile-pictures/hashed/$newfile.jpg"
     fi
 done
